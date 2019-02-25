a5c3de9dc05beb0042d0d77ec42378c51ccbcf59
Alexandra1003 <alexandrapanasenko10.03@gmail.com>
2019-02-25 13:49:05 +0200

    fix/service *move getRate function to service

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,27 +11,6 @@
         $scope.currencyList = CurrencyList;
         $scope.commissionList = CommissionList;
 
-        $scope.getRate = (to, from) => {
-          const toUAH = $scope.list.find(el => el.ccy === to).buy;
-          const fromUAH = $scope.list.find(el => el.ccy === from).sale;
-
-          if (to === CurrencyList[3]) {
-            const toUSD = $scope.list.find(el => el.ccy === CurrencyList[3]).buy;
-            const toUAH = $scope.list.find(el => el.ccy === CurrencyList[0]).buy;
-
-            return (toUSD * toUAH / fromUAH).toFixed(4);
-          }
-
-          if (from === CurrencyList[3]) {
-            const toUSD = $scope.list.find(el => el.ccy === CurrencyList[0]).sale;
-            const fromUSD = $scope.list.find(el => el.ccy === CurrencyList[3]).sale;
-
-            return (toUAH / toUSD / fromUSD).toFixed(4);
-          }
-
-          return (toUAH / fromUAH).toFixed(4);
-        };
-
         $scope.getResultCur = (inCurr = 0, rate, commission) => {
           const commissionSum = inCurr * rate * commission / 100;
 
@@ -55,8 +34,8 @@
         };
 
         $scope.updateCurrValue = () => {
-          $scope.rateBuy = $scope.getRate($scope.fieldSell, $scope.fieldBuy);
-          $scope.rateSell = $scope.getRate($scope.fieldBuy, $scope.fieldSell);
+          $scope.rateBuy = apiService.getRate($scope.fieldSell, $scope.fieldBuy);
+          $scope.rateSell = apiService.getRate($scope.fieldBuy, $scope.fieldSell);
           $scope.changeBuyInput();
         };
 
@@ -64,18 +43,10 @@
           $scope.changeBuyInput();
         };
 
-        apiService.getExchangeRate().then(data => {
+        apiService.getRateList().then(data => {
           $scope.list = data;
-          $scope.rateBuy = $scope.getRate($scope.fieldSell, $scope.fieldBuy);
-          $scope.rateSell = $scope.getRate($scope.fieldBuy, $scope.fieldSell);
+          $scope.rateBuy = apiService.getRate($scope.fieldSell, $scope.fieldBuy);
+          $scope.rateSell = apiService.getRate($scope.fieldBuy, $scope.fieldSell);
         });
       }]);
-
-  window.app.filter('filterCurrency', [function() {
-    return function(array, expression) {
-      return array.filter(function(item) {
-        return !expression || !angular.equals(item, expression);
-      });
-    };
-  }]);
 })();
--- src/js/service.js
+++ src/js/service.js
@@ -5,14 +5,43 @@
   window.app.constant('CommissionList', [0, 1, 2, 5, 10]);
 
   window.app.service('apiService', ['$http', function($http) {
-    this.exchangeRate = null;
-    this.getExchangeRate = () => {
+    this.listOfRates = null;
+    this.getRateList = () => {
       return $http({
         method: 'GET',
         url: 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
       }).then(({ data }) => {
-        this.exchangeRate = data;
-        return this.exchangeRate;
+        this.listOfRates = data;
+        return this.listOfRates;
+      });
+    };
+
+    this.getRate = (from, to) => {
+      const toUAH = this.listOfRates.find(el => el.ccy === from).buy;
+      const fromUAH = this.listOfRates.find(el => el.ccy === to).sale;
+
+      if (from === this.listOfRates[3].ccy) {
+        const toUSD = this.listOfRates.find(el => el.ccy === this.listOfRates[3].ccy).buy;
+        const toUAH = this.listOfRates.find(el => el.ccy === this.listOfRates[0].ccy).buy;
+
+        return (toUSD * toUAH / fromUAH).toFixed(4);
+      }
+
+      if (to === this.listOfRates[3].ccy) {
+        const toUSD = this.listOfRates.find(el => el.ccy === this.listOfRates[0].ccy).sale;
+        const fromUSD = this.listOfRates.find(el => el.ccy === this.listOfRates[3].ccy).sale;
+
+        return (toUAH / toUSD / fromUSD).toFixed(4);
+      }
+
+      return (toUAH / fromUAH).toFixed(4);
+    };
+  }]);
+
+  window.app.filter('filterCurrency', [function() {
+    return function(array, expression) {
+      return array.filter(function(item) {
+        return !expression || !angular.equals(item, expression);
       });
     };
   }]);
