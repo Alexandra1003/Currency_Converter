688f11a2477295e13edfc6f1886ff7a3a26c23ea
Alexandra1003 <alexandrapanasenko10.03@gmail.com>
2019-02-27 15:18:24 +0200

    fix component

--- src/js/directive.js
+++ src/js/directive.js
@@ -1,7 +1,7 @@
 (function() {
   window.app.directive('noConnection', function() {
     return {
-      template: '<div class="connection-message" ng-if="!connectionStatus">No connection ...</div>',
+      template: '<div class="connection-message" ng-hide="connectionStatus">No connection ...</div>',
       restrict: 'E',
       controller: 'CurrencyController'
     };
